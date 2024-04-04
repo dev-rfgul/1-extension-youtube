b55fe01fc73c8fd5dd1b5d245b80f4f192fe2dcf
Muhammad Fahad <rfgul587@gmail.com>
2024-04-04 14:17:50 +0500

    completed the project as it displays the 1st video on the youtube page

--- popup.js
+++ popup.js
@@ -1,8 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var playButton = document.getElementById('playButton');
-  
-    playButton.addEventListener('click', function() {
-      chrome.tabs.create({ url: 'https://www.youtube.com/?autoplay=1' });
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("openVideo").addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: () => {
+          document.querySelectorAll("#thumbnail > yt-image > img")[0].click();
+          console.log("clicked");
+        },
+      });
     });
   });
-  
+});
